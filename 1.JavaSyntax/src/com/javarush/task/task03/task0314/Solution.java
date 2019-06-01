f28dfac50be640a36dc5c5a3e2804d71f780ef65
Robert <dominant92@yandex.ru>
2019-06-01 13:27:46 +0400

    коммит solution

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0314/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        for (int i=1; i<=10; i++) {
+            for (int j=1; j<=10; j++)
+                System.out.print(i*j + " ");
+            System.out.println();
+
+
+        //for (int i = 1; i <= 20; i*=2) {
+           // System.out.print(i + " ");
+
+        }
+    }
+}
